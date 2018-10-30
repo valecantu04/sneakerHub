d2071c1308eeae5108f4b7f2b454c2863fa4e58b
Valentin Cantu <MeroMero@Valentins-MacBook-Pro.local>
2018-10-30 12:23:59 -0500

    sneakerHub files
    commit contains .py file that runs on flask. has 2 html routes and a layout.html file

---
+++ sneakerHub.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+posts = [
+    {
+    'author' : 'Valentin Cantu',
+    'title' : 'First blog post',
+    'content' : 'Im making a website',
+    'date_posted' : 'October 26, 2018'
+    },
+    {
+    'author' : 'Valentin Cantu',
+    'title' : 'Second blog post',
+    'content' : 'This is dummy data',
+    'date_posted' : 'October 26, 2018'
+    }
+]
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About SneakerHub')
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
