b3d2941952352c6f2426c1bdd54a6437b6bb7c2e
Valentin Cantu <MeroMero@Valentins-MacBook-Pro.local>
2018-11-06 13:26:44 -0600

    created teplates and general functionality for the registration and login forms using bootstrap

--- sneakerHub.py
+++ sneakerHub.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '28de2b94c37cfc019aea54bd566e012f'
 
 posts = [
     {
@@ -26,6 +29,26 @@ def home():
 def about():
     return render_template('about.html', title='About SneakerHub')
 
+
+@app.route("/register", methods=['GET','POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register SneakerHub Account', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('you have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Failed', 'danger')
+    return render_template('login.html', title='Login to SneakerHub', form=form)
 
 
 if __name__ == '__main__':
