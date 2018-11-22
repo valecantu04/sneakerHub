2af3e0530f662c7fbc4ef4130936a2dbe331e371
Valentin Cantu <MeroMero@Valentins-MacBook-Pro.local>
2018-11-22 13:44:55 -0600

    html and CSS changes

--- sneakerhub/posts/routes.py
+++ sneakerhub/posts/routes.py
@@ -12,8 +12,7 @@ posts = Blueprint('posts', __name__)
 def new_post():
     form = PostForm()
     if form.validate_on_submit():
-        post = Post(title=form.title.data,
-                    content=form.content.data, author=current_user)
+        post = Post(title=form.title.data,content=form.content.data, author=current_user)
         db.session.add(post)
         db.session.commit()
         flash('Your post has been created!', 'success')
